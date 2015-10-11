e0cc6a896403b8929c6e6b9b718fe6a6d544938a
podonoghue <podonoghue@swin.edu.au>
2015-10-11 19:16:52 +1100

    V4.12.1.40 - Reorganised Web page

--- Documentation/Source/MakeDocumentation.cmd
+++ Documentation/Source/MakeDocumentation.cmd
@@ -2,6 +2,10 @@
 
 SET FILES=__USBDM_V4 __USBDM_FlashProgrammers _USBDM_CF_JMxx _USBDM_CF_JS16 _USBDM_Freedom _USBDM_JB16 _USBDM_JMxx _USBDM_JS16 
 
+if not exist ..\External mkdir ..\External
+if not exist ..\External\USBDM mkdir ..\External\USBDM
+if not exist ..\External\USBDM_V4.12 mkdir ..\External\USBDM_V4.12
+
 for %%f in (%FILES%) do doxygen %%f.doxyfile
 
 
