fb4007deeebc33c9055c0c1a557bdd12387d16b1
podonoghue <podonoghue@swin.edu.au>
2015-12-12 00:48:03 +1100

    V4.12.1.60 - CPP Stationery - Added PCA9685 PWM chip

--- PackageFiles/Stationery/Packages/180.ARM_Peripherals/Project_Headers/PCA9685.h
+++ PackageFiles/Stationery/Packages/180.ARM_Peripherals/Project_Headers/PCA9685.h
@@ -1,84 +1,91 @@
-/*
- * pca9685.h
+/**
+ * @file     pca9685.h
+ * @brief    Interface for PCA9685 PWM controller
  *
- *  Created on: 24/06/2014
- *      Author: podonoghue
+ * @version  V4.12.1.50
+ * @date     18 June 2015
  */
 
-#ifndef H_
-#define H_
+#ifndef INCLUDE_USBDM_PCA9685_H
+#define INCLUDE_USBDM_PCA9685_H
 
 #include <stdint.h>
 #include <stddef.h>
 #include "i2c.h"
 
+namespace USBDM {
+
+/**
+ * @addtogroup PCA9685_Group PCA9685 PWM controller
+ * @brief C++ Class providing interface to PCA9685
+ * @{
+ */
+
 // Default address of first (only) motor board
-#define PCA9685_DEFAULT_SLAVE_BASE_ADDRESS   ((uint8_t)(0x60<<1)) // PCA9685 Slave address
+constexpr uint32_t  PCA9685_DEFAULT_SLAVE_BASE_ADDRESS  = ((uint8_t)(0x60<<1)); // PCA9685 Slave address
 
 // Mode register 1, MODE1
-#define PCA9685_MODE1_RESTART_MASK    (1<<7) // 0*/1 => Shows state of RESTART logic.
-#define PCA9685_MODE1_EXTCLK_MASK     (1<<6) // 0*/1 => Use internal/EXTCLK pin clock.
-#define PCA9685_MODE1_AI_MASK         (1<<5) // 0*/1 => Register Auto-Increment enable/disabled.
-#define PCA9685_MODE1_SLEEP_MASK      (1<<4) // 0/1* => Low power mode on/off (Oscillator on/off)
-#define PCA9685_MODE1_SUB1_MASK       (1<<3) // 0/1* => PCA9685 responds to I2C-bus sub-address 1.
-#define PCA9685_MODE1_SUB2_MASK       (1<<2) // 0/1* => PCA9685 responds to I2C-bus sub-address 1.
-#define PCA9685_MODE1_SUB3_MASK       (1<<1) // 0/1* => PCA9685 responds to I2C-bus sub-address 1.
-#define PCA9685_MODE1_ALLCALL_MASK    (1<<0) // 0/1* => Does not/does respond to LED All Call I2C-bus address.
+constexpr uint32_t  PCA9685_MODE1_RESTART_MASK  =  (1<<7); // 0*/1 => Shows state of RESTART logic.
+constexpr uint32_t  PCA9685_MODE1_EXTCLK_MASK   =  (1<<6); // 0*/1 => Use internal/EXTCLK pin clock.
+constexpr uint32_t  PCA9685_MODE1_AI_MASK       =  (1<<5); // 0*/1 => Register Auto-Increment enable/disabled.
+constexpr uint32_t  PCA9685_MODE1_SLEEP_MASK    =  (1<<4); // 0/1* => Low power mode on/off (Oscillator on/off)
+constexpr uint32_t  PCA9685_MODE1_SUB1_MASK     =  (1<<3); // 0/1* => PCA9685 responds to I2C-bus sub-address 1.
+constexpr uint32_t  PCA9685_MODE1_SUB2_MASK     =  (1<<2); // 0/1* => PCA9685 responds to I2C-bus sub-address 1.
+constexpr uint32_t  PCA9685_MODE1_SUB3_MASK     =  (1<<1); // 0/1* => PCA9685 responds to I2C-bus sub-address 1.
+constexpr uint32_t  PCA9685_MODE1_ALLCALL_MASK  =  (1<<0); // 0/1* => Does not/does respond to LED All Call I2C-bus address.
 
 // Mode register 2, MODE2
-#define PCA9685_MODE2_INVRT_MASK      (1<<4) // 0*   => Output logic state not inverted. Value to use when external driver used.
+constexpr uint32_t  PCA9685_MODE2_INVRT_MASK   =   (1<<4); // 0*   => Output logic state not inverted. Value to use when external driver used.
 //                                           //         Applicable when OE = 0.
 //                                           // 1    => Output logic state inverted. Value to use when no external driver used.
 //                                           //         Applicable when OE = 0.
-#define PCA9685_MODE2_OCH_MASK        (1<<3) // 0*   => Outputs change on STOP command.
+constexpr uint32_t  PCA9685_MODE2_OCH_MASK     =   (1<<3); // 0*   => Outputs change on STOP command.
 //                                           // 1    => Outputs change on ACK.
-#define PCA9685_MODE2_OUTDRV_MASK     (1<<2) // 0    => The 16 LEDn outputs are configured with an open-drain structure.
+constexpr uint32_t  PCA9685_MODE2_OUTDRV_MASK  =   (1<<2); // 0    => The 16 LEDn outputs are configured with an open-drain structure.
 //                                           // 1*   => The 16 LEDn outputs are configured with a totem pole structure.
-#define PCA9685_MODE2_OUTNE_MASK      (3<<0) // 00*  => When OE = 1 (output drivers not enabled), LEDn = 0.
+constexpr uint32_t  PCA9685_MODE2_OUTNE_MASK   =   (3<<0); // 00*  => When OE = 1 (output drivers not enabled), LEDn = 0.
 //                                           // 01   => When OE = 1 (output drivers not enabled):
 //                                           //         LEDn = 1 when OUTDRV = 1
 //                                           //         LEDn = high-impedance when OUTDRV = 0 (same as OUTNE[1:0] = 10)
 //                                           // 1X   => When OE = 1 (output drivers not enabled), LEDn = high-impedance.
 
-// LEDx_ON_L
-//             7:0 LEDx_ON count for LEDx, 8 LSBs
-#define PCA9685_LEDx_ON_L_COUNT_MASK  (0xFF<<0)
-// LEDx_ON_H
-#define PCA9685_LEDx_ON_H_FULL_MASK   (1<<4)
-#define PCA9685_LEDx_ON_H_COUNT_MASK  (0xF<<0)
-//               4 LEDx full ON
-//             3:0 LEDx_ON count for LEDx, 4 MSBs
-// LEDx_OFF_L
-#define PCA9685_LEDx_OFF_L_COUNT_MASK  (0xFF<<0)
-//             7:0 LEDx_OFF count for LEDx, 8 LSBs
-// LEDx_OFF_H
-#define PCA9685_LEDx_OFF_H_FULL_MASK   (1<<4)
-#define PCA9685_LEDx_OFF_H_COUNT_MASK  (0xF<<0)
-//               4 LEDx full OFF
-//             3:0 LEDx_OFF count for LEDx, 4 MSBs
-
 // Default PWM oscillator parameters
-#define PCA9685_OSC_CLOCK_FREQ (25000000UL)
-#define PCA9685_PWM_FREQ       (200UL)
-#define PCA9685_DEFAULT_OSC_PRESCALE   (((PCA9685_OSC_CLOCK_FREQ+2048UL*PCA9685_PWM_FREQ)/(4096UL*PCA9685_PWM_FREQ))-1)
-#if ((PCA9685_OSC_PRESCALE & ~0xFFU) != 0)
-#error "OSC_PRESCALE is too large"
-#endif
+constexpr uint32_t  PCA9685_OSC_CLOCK_FREQ         = (25000000UL);
+constexpr uint32_t  PCA9685_PWM_FREQ               = (200UL);
+constexpr uint32_t  PCA9685_DEFAULT_OSC_PRESCALE   = (((PCA9685_OSC_CLOCK_FREQ+2048UL*PCA9685_PWM_FREQ)/(4096UL*PCA9685_PWM_FREQ))-1);
 
-// First Pin Control register
-#define PCA9685_PIN_CONTROL_BASE        (0x06)   // READ/WRITE LED0  output and brightness control byte 0
-
-// Calculates base register address for pin register set
-#define PCA9685_PIN_REG(pinNum)  ((uint8_t)(PCA9685_PIN_CONTROL_BASE+4*(pinNum)))
-
+/**
+ * @brief Class interfacing a PCA9685 PWM controller chip (such as on the Adafruit motor driver board)
+ *
+ * <b>Example</b>
+ * @code
+ *
+ *    USBDM::I2c *i2c = new USBDM::I2c0();
+ *    USBDM::PCA9685 *pca9685= new USBDM::PCA9685(i2c);
+ *
+ *    pca9685->set_pin_high(3);
+ *    pca9685->set_pin_pwm(3, 50);
+ *
+ * @endcode
+ *
+ */
 class PCA9685 {
 
-protected:
-   I2C     *i2cInterface;
-   uint8_t  slaveAddress;
+private:
+   static const int MAX_PWM = 4095;
 
-   /*!
+   uint8_t  slaveAddress;
+   I2c     *i2cInterface;
+
+public:
+   /**
     * Constructor with default values
+    *
+    * <b>Example</b>
+    * @code
+    *  USBDM::I2c *i2c = new USBDM::I2c0();
+    *  USBDM::PCA9685 *pca9685= new USBDM::PCA9685(i2c);
+    * @endcode
     *
     * @param i2cInterface  - The I2C interface to use
     * @param deviceAddress - Slave device address (PCA9685_DEFAULT_SLAVE_BASE_ADDRESS+0,1,2,3)
@@ -86,20 +93,46 @@ protected:
     * @param mode1Value    - Mode 1 value (use PCA9685_MODE1_x macros)
     * @param mode2Value    - Mode 2 value (use PCA9685_MODE2_x macros)
     */
-   PCA9685(I2C     *i2cInterface,
-           uint8_t  deviceAddress = PCA9685_DEFAULT_SLAVE_BASE_ADDRESS,
-           uint8_t  prescaleValue = PCA9685_DEFAULT_OSC_PRESCALE,
-           uint8_t  mode1Value    = PCA9685_MODE1_AI_MASK,
-           uint8_t  mode2Value    = PCA9685_MODE2_OUTDRV_MASK
-           );
-
-   static const int MAX_PWM = 4095;  // Maximum duty cycle value MAX_PWM = 100%
-
+   PCA9685(I2c *i2cInterface,
+         uint8_t  deviceAddress = PCA9685_DEFAULT_SLAVE_BASE_ADDRESS,
+         uint8_t  prescaleValue = PCA9685_DEFAULT_OSC_PRESCALE,
+         uint8_t  mode1Value    = PCA9685_MODE1_AI_MASK,
+         uint8_t  mode2Value    = PCA9685_MODE2_OUTDRV_MASK
+   );
+   /**
+    * Set all outputs low
+    */
    void allLow(void);
+   /**
+    * Set all outputs high
+    */
    void allHigh(void);
+   /**
+    * Sets the dutyCycle of the given pin
+    *
+    * @param pinNum     Pin to modify (0-15)
+    * @param dutyCycle  Duty-cycle to set. Expressed as a value [0..4095]
+    *
+    */
    void set_pin_pwm(unsigned pinNum, unsigned dutyCycle);
+   /**
+    * Set given pin high
+    *
+    * @param pinNum     Pin to modify (0-15)
+    */
    void set_pin_high(unsigned pinNum);
+   /**
+    * Set given pin low
+    *
+    * @param pinNum     Pin to modify (0-15)
+    */
    void set_pin_low(unsigned pinNum);
 };
 
-#endif /* H_ */
+/**
+ * @}
+ */
+
+} // End namespace USBDM
+
+#endif /* INCLUDE_USBDM_PCA9685_H */
--- PackageFiles/Stationery/Packages/180.ARM_Peripherals/Sources/PCA9685.cpp
+++ PackageFiles/Stationery/Packages/180.ARM_Peripherals/Sources/PCA9685.cpp
@@ -1,118 +1,71 @@
-/*
- * @file lptmr.cpp (derived from pca9685.cpp)
+/**
+ * @file pca9685.cpp
  *
+ *  Created on: 12/12/2015
+ *      Author: podonoghue
  */
-#include "derivative.h"
-#include "utilities.h"
-#include "i2c.h"
 #include "pca9685.h"
+#include <assert.h>
 
-using namespace USBDM;
+namespace USBDM {
 
-#define I2C_SWRST_ADDRESS        (0x00<<1) // General I2C Software reset address
+constexpr uint32_t I2C_SWRST_ADDRESS      =  (0x00<<1); // General I2C Software reset address
 
 //============== PCA9685 register addresses
-#define PCA9685_RESET            (0x06)   // Used in conjunction with I2C_SWRST_ADDR
+constexpr uint32_t PCA9685_RESET          =  (0x06);   // Used in conjunction with I2C_SWRST_ADDR
 
-#define PCA9685_MODE1            (0x00)   // READ/WRITE Mode register 1
-#define PCA9685_MODE2            (0x01)   // READ/WRITE Mode register 2
-#define PCA9685_SUBADR1          (0x02)   // READ/WRITE I2C-bus sub-address 1
-#define PCA9685_SUBADR2          (0x03)   // READ/WRITE I2C-bus sub-address 2
-#define PCA9685_SUBADR3          (0x04)   // READ/WRITE I2C-bus sub-address 3
-#define PCA9685_ALLCALLADR       (0x05)   // READ/WRITE LED All Call I2C-bus address
-#define PCA9685_LED0_ON_L        (0x06)   // READ/WRITE LED0  output and brightness control byte 0
-#define PCA9685_LED0_ON_H        (0x07)   // READ/WRITE LED0  output and brightness control byte 1
-#define PCA9685_LED0_OFF_L       (0x08)   // READ/WRITE LED0  output and brightness control byte 2
-#define PCA9685_LED0_OFF_H       (0x09)   // READ/WRITE LED0  output and brightness control byte 3
-#define PCA9685_LED1_ON_L        (0x0A)   // READ/WRITE LED1  output and brightness control byte 0
-#define PCA9685_LED1_ON_H        (0x0B)   // READ/WRITE LED1  output and brightness control byte 1
-#define PCA9685_LED1_OFF_L       (0x0C)   // READ/WRITE LED1  output and brightness control byte 2
-#define PCA9685_LED1_OFF_H       (0x0D)   // READ/WRITE LED1  output and brightness control byte 3
-#define PCA9685_LED2_ON_L        (0x0E)   // READ/WRITE LED2  output and brightness control byte 0
-#define PCA9685_LED2_ON_H        (0x0F)   // READ/WRITE LED2  output and brightness control byte 1
-#define PCA9685_LED2_OFF_L       (0x10)   // READ/WRITE LED2  output and brightness control byte 2
-#define PCA9685_LED2_OFF_H       (0x11)   // READ/WRITE LED2  output and brightness control byte 3
-#define PCA9685_LED3_ON_L        (0x12)   // READ/WRITE LED3  output and brightness control byte 0
-#define PCA9685_LED3_ON_H        (0x13)   // READ/WRITE LED3  output and brightness control byte 1
-#define PCA9685_LED3_OFF_L       (0x14)   // READ/WRITE LED3  output and brightness control byte 2
-#define PCA9685_LED3_OFF_H       (0x15)   // READ/WRITE LED3  output and brightness control byte 3
-#define PCA9685_LED4_ON_L        (0x16)   // READ/WRITE LED4  output and brightness control byte 0
-#define PCA9685_LED4_ON_H        (0x17)   // READ/WRITE LED4  output and brightness control byte 1
-#define PCA9685_LED4_OFF_L       (0x18)   // READ/WRITE LED4  output and brightness control byte 2
-#define PCA9685_LED4_OFF_H       (0x19)   // READ/WRITE LED4  output and brightness control byte 3
-#define PCA9685_LED5_ON_L        (0x1A)   // READ/WRITE LED5  output and brightness control byte 0
-#define PCA9685_LED5_ON_H        (0x1B)   // READ/WRITE LED5  output and brightness control byte 1
-#define PCA9685_LED5_OFF_L       (0x1C)   // READ/WRITE LED5  output and brightness control byte 2
-#define PCA9685_LED5_OFF_H       (0x1D)   // READ/WRITE LED5  output and brightness control byte 3
-#define PCA9685_LED6_ON_L        (0x1E)   // READ/WRITE LED6  output and brightness control byte 0
-#define PCA9685_LED6_ON_H        (0x1F)   // READ/WRITE LED6  output and brightness control byte 1
-#define PCA9685_LED6_OFF_L       (0x20)   // READ/WRITE LED6  output and brightness control byte 2
-#define PCA9685_LED6_OFF_H       (0x21)   // READ/WRITE LED6  output and brightness control byte 3
-#define PCA9685_LED7_ON_L        (0x22)   // READ/WRITE LED7  output and brightness control byte 0
-#define PCA9685_LED7_ON_H        (0x23)   // READ/WRITE LED7  output and brightness control byte 1
-#define PCA9685_LED7_OFF_L       (0x24)   // READ/WRITE LED7  output and brightness control byte 2
-#define PCA9685_LED7_OFF_H       (0x25)   // READ/WRITE LED7  output and brightness control byte 3
-#define PCA9685_LED8_ON_L        (0x26)   // READ/WRITE LED8  output and brightness control byte 0
-#define PCA9685_LED8_ON_H        (0x27)   // READ/WRITE LED8  output and brightness control byte 1
-#define PCA9685_LED8_OFF_L       (0x28)   // READ/WRITE LED8  output and brightness control byte 2
-#define PCA9685_LED8_OFF_H       (0x29)   // READ/WRITE LED8  output and brightness control byte 3
-#define PCA9685_LED9_ON_L        (0x2A)   // READ/WRITE LED9  output and brightness control byte 0
-#define PCA9685_LED9_ON_H        (0x2B)   // READ/WRITE LED9  output and brightness control byte 1
-#define PCA9685_LED9_OFF_L       (0x2C)   // READ/WRITE LED9  output and brightness control byte 2
-#define PCA9685_LED9_OFF_H       (0x2D)   // READ/WRITE LED9  output and brightness control byte 3
-#define PCA9685_LED10_ON_L       (0x2E)   // READ/WRITE LED10 output and brightness control byte 0
-#define PCA9685_LED10_ON_H       (0x2F)   // READ/WRITE LED10 output and brightness control byte 1
-#define PCA9685_LED10_OFF_L      (0x30)   // READ/WRITE LED10 output and brightness control byte 2
-#define PCA9685_LED10_OFF_H      (0x31)   // READ/WRITE LED10 output and brightness control byte 3
-#define PCA9685_LED11_ON_L       (0x32)   // READ/WRITE LED11 output and brightness control byte 0
-#define PCA9685_LED11_ON_H       (0x33)   // READ/WRITE LED11 output and brightness control byte 1
-#define PCA9685_LED11_OFF_L      (0x34)   // READ/WRITE LED11 output and brightness control byte 2
-#define PCA9685_LED11_OFF_H      (0x35)   // READ/WRITE LED11 output and brightness control byte 3
-#define PCA9685_LED12_ON_L       (0x36)   // READ/WRITE LED12 output and brightness control byte 0
-#define PCA9685_LED12_ON_H       (0x37)   // READ/WRITE LED12 output and brightness control byte 1
-#define PCA9685_LED12_OFF_L      (0x38)   // READ/WRITE LED12 output and brightness control byte 2
-#define PCA9685_LED12_OFF_H      (0x39)   // READ/WRITE LED12 output and brightness control byte 3
-#define PCA9685_LED13_ON_L       (0x3A)   // READ/WRITE LED13 output and brightness control byte 0
-#define PCA9685_LED13_ON_H       (0x3B)   // READ/WRITE LED13 output and brightness control byte 1
-#define PCA9685_LED13_OFF_L      (0x3C)   // READ/WRITE LED13 output and brightness control byte 2
-#define PCA9685_LED13_OFF_H      (0x3D)   // READ/WRITE LED13 output and brightness control byte 3
-#define PCA9685_LED14_ON_L       (0x3E)   // READ/WRITE LED14 output and brightness control byte 0
-#define PCA9685_LED14_ON_H       (0x3F)   // READ/WRITE LED14 output and brightness control byte 1
-#define PCA9685_LED14_OFF_L      (0x40)   // READ/WRITE LED14 output and brightness control byte 2
-#define PCA9685_LED14_OFF_H      (0x41)   // READ/WRITE LED14 output and brightness control byte 3
-#define PCA9685_LED15_ON_L       (0x42)   // READ/WRITE LED15 output and brightness control byte 0
-#define PCA9685_LED15_ON_H       (0x43)   // READ/WRITE LED15 output and brightness control byte 1
-#define PCA9685_LED15_OFF_L      (0x44)   // READ/WRITE LED15 output and brightness control byte 2
-#define PCA9685_LED15_OFF_H      (0x45)   // READ/WRITE LED15 output and brightness control byte 3
+constexpr uint32_t PCA9685_MODE1          =  (0x00);   // READ/WRITE Mode register 1
+constexpr uint32_t PCA9685_MODE2          =  (0x01);   // READ/WRITE Mode register 2
+constexpr uint32_t PCA9685_SUBADR1        =  (0x02);   // READ/WRITE I2C-bus sub-address 1
+constexpr uint32_t PCA9685_SUBADR2        =  (0x03);   // READ/WRITE I2C-bus sub-address 2
+constexpr uint32_t PCA9685_SUBADR3        =  (0x04);   // READ/WRITE I2C-bus sub-address 3
+constexpr uint32_t PCA9685_ALLCALLADR     =  (0x05);   // READ/WRITE OUT All Call I2C-bus address
+constexpr uint32_t PCA9685_OUT_START      =  (0x06);   // READ/WRITE OUT0  output and brightness control byte 0
+//constexpr uint32_t PCA9685_OUT0_ON_L       = (0x06);   // READ/WRITE OUT0  output and brightness control byte 0
+//constexpr uint32_t PCA9685_OUT0_ON_H       = (0x07);   // READ/WRITE OUT0  output and brightness control byte 1
+//constexpr uint32_t PCA9685_OUT0_OFF_L      = (0x08);   // READ/WRITE OUT0  output and brightness control byte 2
+//constexpr uint32_t PCA9685_OUT0_OFF_H      = (0x09);   // READ/WRITE OUT0  output and brightness control byte 3
 //...
-#define PCA9685_ALL_LED_ON_L     (0xFA)   // READ 0/WRITE load all the LEDn_ON  registers, byte 0
-#define PCA9685_ALL_LED_ON_H     (0xFB)   // READ 0/WRITE load all the LEDn_ON  registers, byte 1
-#define PCA9685_ALL_LED_OFF_L    (0xFC)   // READ 0/WRITE load all the LEDn_OFF registers, byte 2
-#define PCA9685_ALL_LED_OFF_H    (0xFD)   // READ 0/WRITE load all the LEDn_OFF registers, byte 3
+constexpr uint32_t PCA9685_ALL_OUT_ON_L   =  (0xFA);   // READ 0/WRITE load all the OUTn_ON  registers, byte 0
+constexpr uint32_t PCA9685_ALL_OUT_ON_H   =  (0xFB);   // READ 0/WRITE load all the OUTn_ON  registers, byte 1
+constexpr uint32_t PCA9685_ALL_OUT_OFF_L  =  (0xFC);   // READ 0/WRITE load all the OUTn_OFF registers, byte 2
+constexpr uint32_t PCA9685_ALL_OUT_OFF_H  =  (0xFD);   // READ 0/WRITE load all the OUTn_OFF registers, byte 3
 
-#define PCA9685_PRE_SCALE        (0xFE)   // READ/WRITE pre-scaler for output frequency
-#define PCA9685_TestMode         (0xFF)   // READ/WRITE defines the test mode to be entered
+constexpr uint32_t PCA9685_PRE_SCALE      =  (0xFE);   // READ/WRITE pre-scaler for output frequency
+constexpr uint32_t PCA9685_TestMode       =  (0xFF);   // READ/WRITE defines the test mode to be entered
 
-// LEDx_ON_L
-//             7:0 LEDx_ON count for LEDx, 8 LSBs
-#define LEDx_ON_L_COUNT_MASK  (0xFF<<0)
-// LEDx_ON_H
-#define LEDx_ON_H_FULL_MASK   (1<<4)
-#define LEDx_ON_H_COUNT_MASK  (0xF<<0)
-//               4 LEDx full ON
-//             3:0 LEDx_ON count for LEDx, 4 MSBs
-// LEDx_OFF_L
-#define LEDx_OFF_L_COUNT_MASK  (0xFF<<0)
-//             7:0 LEDx_OFF count for LEDx, 8 LSBs
-// LEDx_OFF_H
-#define LEDx_OFF_H_FULL_MASK   (1<<4)
-#define LEDx_OFF_H_COUNT_MASK  (0xF<<0)
-//               4 LEDx full OFF
-//             3:0 LEDx_OFF count for LEDx, 4 MSBs
+// Calculates base register address for pin register set
+constexpr inline int PCA9685_PIN_REG(int pinNum) {
+   return ((uint8_t)(PCA9685_OUT_START+4*(pinNum)));
+}
 
-#define ALL_CALL_ADDDRESS (0xE0)
+// OUTx_ON_L
+//             7:0 OUTx_ON count for OUTx, 8 LSBs
+constexpr uint32_t OUTx_ON_L_COUNT_MASK = (0xFF<<0);
+// OUTx_ON_H
+constexpr uint32_t OUTx_ON_H_FULL_MASK  = (1<<4);
+constexpr uint32_t OUTx_ON_H_COUNT_MASK = (0xF<<0);
+//               4 OUTx full ON
+//             3:0 OUTx_ON count for OUTx, 4 MSBs
+// OUTx_OFF_L
+constexpr uint32_t OUTx_OFF_L_COUNT_MASK = (0xFF<<0);
+//             7:0 OUTx_OFF count for OUTx, 8 LSBs
+// OUTx_OFF_H
+constexpr uint32_t OUTx_OFF_H_FULL_MASK  = (1<<4);
+constexpr uint32_t OUTx_OFF_H_COUNT_MASK = (0xF<<0);
+//               4 OUTx full OFF
+//             3:0 OUTx_OFF count for OUTx, 4 MSBs
 
-/*!
+constexpr uint32_t ALL_CALL_ADDDRESS  =  (0xE0);
+
+inline void assert_helper( bool test ) {
+  assert(test);
+}
+inline constexpr bool constexpr_assert( bool test ) {
+  return test?true:(assert_helper(test),false);
+}
+
+/**
  * Constructor with default values
  *
  * @param deviceAddress    Device I2C address
@@ -121,18 +74,17 @@ using namespace USBDM;
  * @param mode2Value       Mode Register 2 value
  *
  */
-PCA9685::PCA9685(I2C *i2cInterface, uint8_t slaveAddress, uint8_t prescaleValue, uint8_t mode1Value, uint8_t mode2Value) {
-   this->slaveAddress = slaveAddress;
+PCA9685::PCA9685(I2c *i2cInterface, uint8_t deviceAddress, uint8_t prescaleValue, uint8_t mode1Value, uint8_t mode2Value) {
+   slaveAddress = deviceAddress;
 
-   if (!i2cInterface) {
-      i2cInterface = new I2C_0(0x00);
-   }
    this->i2cInterface = i2cInterface;
 
-//   uint8_t resetdata[] = {RESET};
+   assert((PCA9685_DEFAULT_OSC_PRESCALE & ~0xFFU) == 0); // Check if OSC_PRESCALE is too large
+
+//   uint8_t resetdata[] = {PCA9685_RESET};
 //   i2c_transmit(I2C_SWRST_ADDRESS, resetdata, sizeof(resetdata));
    //
-   uint8_t mode1data[] = {
+   const uint8_t mode1data[] = {
        /* register address */   PCA9685_MODE1,
        /* mode1            */   mode1Value,
        /* mode2            */   mode2Value,
@@ -143,7 +95,7 @@ PCA9685::PCA9685(I2C *i2cInterface, uint8_t slaveAddress, uint8_t prescaleValue,
    };
    i2cInterface->transmit(slaveAddress, mode1data, sizeof(mode1data));
    //
-   uint8_t prescaleData[] = {
+   const uint8_t prescaleData[] = {
       PCA9685_PRE_SCALE,
       prescaleValue   // Prescale value for clock
    };
@@ -152,85 +104,87 @@ PCA9685::PCA9685(I2C *i2cInterface, uint8_t slaveAddress, uint8_t prescaleValue,
    allHigh();
 }
 
-/*!
+/**
  * Set all outputs high
  */
 void PCA9685::allHigh(void) {
-   uint8_t data[] = {
-      /* register */ (uint8_t) PCA9685_ALL_LED_ON_L,
+   const uint8_t data[] = {
+      /* register */ (uint8_t) PCA9685_ALL_OUT_ON_L,
       /* onLow    */ (uint8_t) 0,
       /* onHigh   */ (uint8_t) 0,
       /* offLow   */ (uint8_t) 0,
-      /* offHigh  */ (uint8_t) LEDx_OFF_H_FULL_MASK,
+      /* offHigh  */ (uint8_t) OUTx_OFF_H_FULL_MASK,
    };
    i2cInterface->transmit(ALL_CALL_ADDDRESS, data, sizeof(data));
 }
 
-/*!
+/**
  * Set all outputs low
  */
 void PCA9685::allLow(void) {
-   uint8_t data[] = {
-      /* register */ (uint8_t) PCA9685_ALL_LED_ON_L,
+   const uint8_t data[] = {
+      /* register */ (uint8_t) PCA9685_ALL_OUT_ON_L,
       /* onLow    */ (uint8_t) 0,
-      /* onHigh   */ (uint8_t) LEDx_ON_H_FULL_MASK,
+      /* onHigh   */ (uint8_t) OUTx_ON_H_FULL_MASK,
       /* offLow   */ (uint8_t) 0,
       /* offHigh  */ (uint8_t) 0,
    };
    i2cInterface->transmit(ALL_CALL_ADDDRESS, data, sizeof(data));
 }
 
-/*!
+/**
  * Sets the dutyCycle of the given pin
  *
  * @param pinNum     Pin to modify
- * @param dutyCycle  Duty-cycle to set (0-4095)
+ * @param dutyCycle  Duty-cycle to set. Expressed as a value [0..4095]
  *
  */
 void PCA9685::set_pin_pwm(unsigned pinNum, unsigned dutyCycle) {
    if (dutyCycle>MAX_PWM) {
       dutyCycle = MAX_PWM;
    }
-   uint16_t onCount  = 0;
-   uint16_t offCount = dutyCycle;
-   uint8_t data[] = {
+   const uint16_t onCount  = 0;
+   const uint16_t offCount = dutyCycle;
+   const uint8_t data[] = {
       /* register */ (uint8_t) PCA9685_PIN_REG(pinNum),
       /* onLow    */ (uint8_t) onCount,
-      /* onHigh   */ (uint8_t) ((onCount>>8)&LEDx_ON_H_COUNT_MASK),
+      /* onHigh   */ (uint8_t) ((onCount>>8)&OUTx_ON_H_COUNT_MASK),
       /* offLow   */ (uint8_t) offCount,
-      /* offHigh  */ (uint8_t) ((offCount>>8)&LEDx_OFF_H_COUNT_MASK),
+      /* offHigh  */ (uint8_t) ((offCount>>8)&OUTx_OFF_H_COUNT_MASK),
    };
    i2cInterface->transmit(slaveAddress, data, sizeof(data));
 }
 
-/*!
+/**
  * Set given pin low
  *
  * @param pinNum Pin to change
  */
 void PCA9685::set_pin_low(unsigned pinNum) {
-   uint8_t data[] = {
+   const uint8_t data[] = {
       /* register */ (uint8_t) PCA9685_PIN_REG(pinNum),
       /* onLow    */ (uint8_t) 0,
-      /* onHigh   */ (uint8_t) LEDx_ON_H_FULL_MASK,
+      /* onHigh   */ (uint8_t) OUTx_ON_H_FULL_MASK,
       /* offLow   */ (uint8_t) 0,
       /* offHigh  */ (uint8_t) 0,
    };
    i2cInterface->transmit(slaveAddress, data, sizeof(data));
 }
 
-/*!
+/**
  * Set given pin high
  *
  * @param pinNum Pin to change
  */
 void PCA9685::set_pin_high(unsigned pinNum) {
-   uint8_t data[] = {
+   const uint8_t data[] = {
       /* register */ (uint8_t) PCA9685_PIN_REG(pinNum),
       /* onLow    */ (uint8_t) 0,
       /* onHigh   */ (uint8_t) 0,
       /* offLow   */ (uint8_t) 0,
-      /* offHigh  */ (uint8_t) LEDx_OFF_H_FULL_MASK,
+      /* offHigh  */ (uint8_t) OUTx_OFF_H_FULL_MASK,
    };
    i2cInterface->transmit(slaveAddress, data, sizeof(data));
 }
+
+} // End namespace USBDM
