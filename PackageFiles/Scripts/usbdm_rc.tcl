d7d9aac9589b8b608cf47bfaf09bd1cf5bfc9f7d
podonoghue <podonoghue@swin.edu.au>
2017-04-14 16:50:43 +1000

    V4.12.1.170 - More useful opening description of UsbdmScript

--- PackageFiles/Scripts/usbdm_rc.tcl
+++ PackageFiles/Scripts/usbdm_rc.tcl
@@ -3,10 +3,19 @@ puts ""
 puts "==============================================================="
 puts "  usbdm_rc.tcl  "
 puts "================"
-puts "  This file is 'sourced' whenever usbdmScript.exe is executed  "
-puts "  Add your own custom commands                                 "
+puts "  This file is 'sourced' whenever usbdmScript.exe is executed"
+puts "  Add your own custom commands"
+puts "  Type ? for command summary"
+puts ""
+puts "  Typical sequence:"
+puts "  settarget arm"
+puts "  openbdm   ;#can add number of reported BDM if needed"
+puts "  connect"
+puts "  regs"
 puts "==============================================================="
 puts ""
+;#
+;# Connect command for HCS08 targets
 proc Connect {} {
    ;# Try to connect - if fail prompt for manual power cycle
    ;# This is the only sure way to connect to a some targets
