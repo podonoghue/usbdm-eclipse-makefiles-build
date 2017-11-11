b96d16020633862d69efbe6a50585747e18cb34f
Peter O'Donoghue <podonoghue@swin.edu.au>
2017-11-11 16:43:40 +1100

    V4.12.1.210 - Changed naming of device header file to be consistently the device subfamily e.g. MK20D5.h

--- PackageFiles/Stationery/Packages/100.ARM_DeviceOptions/Project_Headers/derivative.h
+++ PackageFiles/Stationery/Packages/100.ARM_DeviceOptions/Project_Headers/derivative.h
@@ -1,7 +1,7 @@
 /**
  * @file     derivative.h
- * @brief    Include the derivative-specific header file $(targetDevice).h
+ * @brief    Include the derivative-specific header file $(targetDeviceSubFamily).h
  * @version  V4.11.1.70
  * @date     13 Nov 2012
  */
-#include "$(targetDevice).h"
+#include "$(targetDeviceSubFamily).h"
