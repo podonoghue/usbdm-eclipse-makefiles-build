47ef72aa622bd1b9fac9d3c3376d69145a567551
Peter O'Donoghue <podonoghue@swin.edu.au>
2018-07-15 04:35:40 +1000

    V4.12.1.230 - Added basic usbdcd code

---
+++ PackageFiles/Stationery/Packages/180.ARM_Peripherals/Snippets/usbdcd-example.cpp
@@ -0,0 +1,57 @@
+/**
+ ============================================================================
+ * @file   usbdcd-example.cpp (180.ARM_Peripherals/Snippets)
+ * @date   12 Jul 2018
+ * @author podonoghue
+ * @brief  USBDCD Example code
+ ============================================================================
+ */
+
+#include "hardware.h"
+#include "usbdcd.h"
+
+using namespace USBDM;
+
+void callback() {
+   UsbdcdStatus status = Usbdcd::getStatus();
+   Usbdcd::clearInterrupt();
+   if (status.timeout) {
+      console.writeln("Timeout");
+   }
+   else if (status.error) {
+      console.writeln("Error");
+   }
+   else {
+      switch (status.usbdcdSeqStatus) {
+         case UsbdcdSeqStatus_Idle:
+            console.writeln("Idle");
+            break;
+         case UsbdcdSeqStatus_PinsDetected:
+            console.writeln("PinsDetected");
+            break;
+         case UsbdcdSeqStatus_PortDetected:
+            console.writeln("PortDetected");
+            break;
+         case UsbdcdSeqStatus_TypeDetected:
+            console.writeln("TypeDetected");
+            break;
+      }
+   }
+}
+
+int main() {
+   console.writeln("Starting");
+
+   Usbdcd::configure();
+   Usbdcd::setCallback(callback);
+   Usbdcd::enableInterrupt();
+   Usbdcd::enableNvicInterrupts();
+
+   Usbdcd::startDetection();
+
+   for(;;) {
+      __asm__("nop");
+   }
+   return 0;
+}
+
