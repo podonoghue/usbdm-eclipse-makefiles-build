ef9d31cdca14f0aee530e10ea14f828cd8c3a21b
Peter O'Donoghue <podonoghue@swin.edu.au>
2018-07-24 17:28:25 +1000

    V4.12.1.230 - Updated example to use RCM module and more logging

--- PackageFiles/Stationery/Packages/180.ARM_Peripherals/Snippets/nonvolatile-example.cpp
+++ PackageFiles/Stationery/Packages/180.ARM_Peripherals/Snippets/nonvolatile-example.cpp
@@ -1,8 +1,13 @@
-/*
+/**
  ============================================================================
  * @file    nonvolatile-example.cpp (180.ARM_Peripherals)
  * @brief   Basic C++ demo of non-volatile (flexRAM) template class
  *
+ *  Created on: 10/1/2016
+ *      Author: podonoghue
+ ============================================================================
+ */
+/*
  * This example shows how to create a non-volatile variable located in the
  * FlexRAM region of memory and backed by non-volatile storage in the Flash.
  * The variable counts the number of times the chip has been power-on-reset.
@@ -16,42 +21,47 @@
  * Only Release versions of the software will actually use non-volatile storage
  * and operate correctly i.e. the FlexRAM will be backed by Flash storage
  *
- *  Created on: 10/1/2016
- *      Author: podonoghue
- ============================================================================
  */
 #include "system.h"
 #include "derivative.h"
 #include "hardware.h"
 #include "delay.h"
 #include "flash.h"
-
+#ifdef USBDM_RCM_IS_DEFINED
+#include "rcm.h"
+#endif
 using namespace USBDM;
 
-/** Non-volatile variable to count how many times the device has booted */
+/**
+ * Non-volatile variable to count how many times the device has booted.
+ * This must be placed in FlexRAM section.
+ */
 __attribute__ ((section(".flexRAM")))
 Nonvolatile<int> bootCount_nv;
 
 /**
  * A derived class similar to this should be created to do the following:
- * - Configure and partition the flash on the first reset after programming the device.
+ * - Wait for initialisation of the FlexRAM from the Flash backing store - Flash();
+ * - Configure and partition the flash on the first reset after programming the device - initialiseEeprom().
  * - Do once-only initialisation of non-volatile variables when the above occurs.
- * - Initialise the FlexRAM from the Flash backing store
  */
 class NvInit : public Flash {
 public:
    NvInit() : Flash() {
+
       // Initialise the non-volatile system and configure if necessary
-      volatile int rc = initialiseEeprom<EepromSel_2KBytes, PartitionSel_flash0K_eeprom32K, SplitSel_disabled>();
+      volatile FlashDriverError_t rc = initialiseEeprom<EepromSel_2KBytes, PartitionSel_flash0K_eeprom32K, SplitSel_disabled>();
 
       if (rc == FLASH_ERR_NEW_EEPROM) {
          // This is the first reset after programming the device
          // Initialise the non-volatile variables as necessary
          // If not initialised they will have an initial value of 0xFF
          bootCount_nv = 0;
+         console.writeln("Initialising NV variables");
       }
       else {
          usbdm_assert(rc != FLASH_ERR_OK, "FlexNVM initialisation error");
+         console.writeln("Not initialising NV variables");
       }
    }
 };
@@ -60,17 +70,18 @@ int main() {
    // Initialise non-volatile storage
    NvInit nvinit;
 
-#ifdef RCM_SRS0_POR_MASK
-   if ((RCM->SRS0 & RCM_SRS0_POR_MASK) != 0) {
-      // Only count power-on resets
+#ifdef USBDM_RCM_IS_DEFINED
+   // Only count power-on resets
+   if ((Rcm::getResetSource() & RcmSource_Por) != 0) {
       bootCount_nv = bootCount_nv + 1;
+      console.write("Starting, power-on boot count = ").writeln((int)bootCount_nv);
    }
 #else
-   // Count all resets if SRS not available
+   // Count all resets if RCM is not available
    bootCount_nv = bootCount_nv + 1;
+   console.write("Starting, boot count = ").writeln((int)bootCount_nv);
 #endif
 
-   console.write("Starting, power on boot count = ").writeln((int)bootCount_nv);
    for(;;) {
    }
    return 0;
