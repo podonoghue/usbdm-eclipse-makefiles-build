b7f8686e08eb38d3ad2f8a93c9bb4115d11f9b9b
Peter O'Donoghue <podonoghue@swin.edu.au>
2018-07-28 22:41:31 +1000

    V4.12.1.230 - Modified usbdm_assert() to abort program

--- PackageFiles/Stationery/Packages/180.ARM_Peripherals/Project_Headers/hardware.h
+++ PackageFiles/Stationery/Packages/180.ARM_Peripherals/Project_Headers/hardware.h
@@ -211,13 +211,20 @@ extern void log_error(const char *msg);
 #define USBDM_STRINGIFY(x)  #x
 #define USBDM_TOSTRING(x)   USBDM_STRINGIFY(x)
 
+extern "C" void _exit(int);
+
+inline void _usbdm_assert(const char *msg) {
+   USBDM::log_error(msg);
+   _exit(-1);
+}
+
 /**
  * Macro to do ASSERT operation in debug build
  *
  * @param __e Assert expression to evaluate
  * @param __m Message to print if expression is false
  */
-#define USBDM_ASSERT(__e, __m) ((__e) ? (void)0 : (void)USBDM::log_error("Assertion Failed @" __FILE__ ":" USBDM_TOSTRING(__LINE__) " - " __m))
+#define USBDM_ASSERT(__e, __m) ((__e) ? (void)0 : (void)_usbdm_assert("Assertion Failed @" __FILE__ ":" USBDM_TOSTRING(__LINE__) " - " __m))
 #define usbdm_assert(__e, __m) USBDM_ASSERT(__e, __m)
 #else
 /**
