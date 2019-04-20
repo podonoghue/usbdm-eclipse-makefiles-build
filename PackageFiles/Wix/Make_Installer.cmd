6075aec1b9878b6e9ced67e8e831b42255f774d8
pgo <podonoghue@swin.edu.au>
2019-04-20 23:28:11 +1000

    V4.12.1.260 - Changed to have independent Update sites for Codewarrior and Eclipse

--- PackageFiles/Wix/Make_Installer.cmd
+++ PackageFiles/Wix/Make_Installer.cmd
@@ -21,10 +21,11 @@ set PYRO=%WIX_DIR%\pyro.exe
 set WIX_BUILD_DIR=wixBuildDir
 
 rem .wxs regenerated Files 
-set REBUILT_WXS=DeviceData.wxs Examples.wxs FlashImages.wxs Stationery.wxs UpdateSite.wxs WizardPatchData.wxs
+set REBUILT_WXS=DeviceData.wxs Stationery.wxs FlashImages.wxs WizardPatchData.wxs Examples.wxs UpdateSite.wxs
 
 set CANDLE_OPTIONS=-ext WixUIExtension -ext WixUtilExtension
 set LIGHT_OPTIONS=-ext WixUIExtension -ext WixUtilExtension -sw0204 -dWixUIBannerBmp=usbdm.bmp
+rem set LIGHT_DIRS=-b %SRC_DIR%\DeviceData -b %SRC_DIR%\Stationery -b %SRC_DIR%\FlashImages -b %SRC_DIR%\WizardPatches -b %SRC_DIR%\Examples -b %SRC_DIR%\CodewarriorUpdateSite -b %SRC_DIR%\EclipseUpdateSite
 set LIGHT_DIRS=-b %SRC_DIR%\DeviceData -b %SRC_DIR%\Stationery -b %SRC_DIR%\FlashImages -b %SRC_DIR%\WizardPatches -b %SRC_DIR%\Examples -b %SRC_DIR%\UpdateSite
 
 set HEAT_OPTIONS=-srd -ke -gg -sfrag -template fragment -sw5150
@@ -46,12 +47,12 @@ echo Cleaning build directory
 if exist %WIX_BUILD_DIR% rmdir /S /Q %WIX_BUILD_DIR%
 mkdir %WIX_BUILD_DIR%
 
-%HEAT% dir %SRC_DIR%\DeviceData      %HEAT_OPTIONS% -cg Cg.DeviceData      -dr D.DeviceData      -out %WIX_BUILD_DIR%\DeviceData.wxs
-%HEAT% dir %SRC_DIR%\Stationery      %HEAT_OPTIONS% -cg Cg.Stationery      -dr D.Stationery      -out %WIX_BUILD_DIR%\Stationery.wxs
-%HEAT% dir %SRC_DIR%\FlashImages     %HEAT_OPTIONS% -cg Cg.FlashImages     -dr D.FlashImages     -out %WIX_BUILD_DIR%\FlashImages.wxs
-%HEAT% dir %SRC_DIR%\WizardPatches   %HEAT_OPTIONS% -cg Cg.WizardPatchData -dr D.WizardPatchData -out %WIX_BUILD_DIR%\WizardPatchData.wxs
-%HEAT% dir %SRC_DIR%\Examples        %HEAT_OPTIONS% -cg Cg.Examples        -dr D.Examples        -out %WIX_BUILD_DIR%\Examples.wxs
-%HEAT% dir %SRC_DIR%\UpdateSite      %HEAT_OPTIONS% -cg Cg.UpdateSite      -dr D.UpdateSite      -out %WIX_BUILD_DIR%\UpdateSite.wxs
+%HEAT% dir %SRC_DIR%\DeviceData            %HEAT_OPTIONS% -cg Cg.DeviceData            -dr D.DeviceData            -out %WIX_BUILD_DIR%\DeviceData.wxs
+%HEAT% dir %SRC_DIR%\Stationery            %HEAT_OPTIONS% -cg Cg.Stationery            -dr D.Stationery            -out %WIX_BUILD_DIR%\Stationery.wxs
+%HEAT% dir %SRC_DIR%\FlashImages           %HEAT_OPTIONS% -cg Cg.FlashImages           -dr D.FlashImages           -out %WIX_BUILD_DIR%\FlashImages.wxs
+%HEAT% dir %SRC_DIR%\WizardPatches         %HEAT_OPTIONS% -cg Cg.WizardPatchData       -dr D.WizardPatchData       -out %WIX_BUILD_DIR%\WizardPatchData.wxs
+%HEAT% dir %SRC_DIR%\Examples              %HEAT_OPTIONS% -cg Cg.Examples              -dr D.Examples              -out %WIX_BUILD_DIR%\Examples.wxs
+%HEAT% dir %SRC_DIR%\UpdateSite            %HEAT_OPTIONS% -cg Cg.UpdateSite            -dr MANUFACTURER_FOLDER     -out %WIX_BUILD_DIR%\UpdateSite.wxs
 
 %CANDLE% %CANDLE_OPTIONS% -dProductVersion=%VERSIONn% -dWxWidgetsVer=%WXWIDGETS_VERSION% -dSrcDir="%SRC_DIR%" -o %WIX_BUILD_DIR%\ *.wxs %WIX_BUILD_DIR%\*.wxs
 %LIGHT% %LIGHT_OPTIONS% %LIGHT_DIRS% -out %MSI_FILE% %WIX_BUILD_DIR%\*.wixobj
