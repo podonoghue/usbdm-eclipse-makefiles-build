8ca9ed57e0b42f60ec9f2f5b27262934dc95fe48
pgo <podonoghue@swin.edu.au>
2019-06-11 23:13:33 +1000

    V4.12.1.262 - Experiment with other MSI builds

--- PackageFiles/Wix/Make_Utilities_Installer.cmd
+++ PackageFiles/Wix/Make_Utilities_Installer.cmd
@@ -21,14 +21,14 @@ set PYRO=%WIX_DIR%\pyro.exe
 set WIX_BUILD_DIR=wixBuildDir
 
 rem .wxs regenerated Files 
-set REBUILT_WXS=DeviceData.wxs Stationery.wxs FlashImages.wxs WizardPatchData.wxs Examples.wxs UpdateSite.wxs
+set REBUILT_WXS=DeviceData.wxs Stationery.wxs FlashImages.wxs UpdateSite.wxs
 
 set CANDLE_OPTIONS=-ext WixUIExtension -ext WixUtilExtension
 set LIGHT_OPTIONS=-ext WixUIExtension -ext WixUtilExtension -sw0204 -dWixUIBannerBmp=usbdm.bmp
-set LIGHT_DIRS=-b %SRC_DIR%\DeviceData -b %SRC_DIR%\Stationery -b %SRC_DIR%\FlashImages -b %SRC_DIR%\WizardPatches -b %SRC_DIR%\Examples -b %SRC_DIR%\UpdateSite
+set LIGHT_DIRS=-b %SRC_DIR%\DeviceData -b %SRC_DIR%\Stationery -b %SRC_DIR%\FlashImages -b %SRC_DIR%\Examples -b %SRC_DIR%\UpdateSite
 
 set HEAT_OPTIONS=-srd -ke -gg -sfrag -template fragment -sw5150
-set MSI_FILE=USBDM_%VERSION%_Win
+set MSI_FILE_UTILITY=USBDM_Utilities_%VERSION%_Win
 set PATCH=patch_1_1
 
 if "%1"=="patch" goto doPatch
@@ -38,7 +38,7 @@ echo "Unknown option %1"
 goto finish
 
 :doMake
-rem del %MSI_FILE%.msi
+rem del %MSI_FILE_UTILITY%.msi
 
 if "%1"=="clean" goto finish
 
@@ -49,21 +49,20 @@ mkdir %WIX_BUILD_DIR%
 %HEAT% dir %SRC_DIR%\DeviceData            %HEAT_OPTIONS% -cg Cg.DeviceData            -dr D.DeviceData            -out %WIX_BUILD_DIR%\DeviceData.wxs
 %HEAT% dir %SRC_DIR%\Stationery            %HEAT_OPTIONS% -cg Cg.Stationery            -dr D.Stationery            -out %WIX_BUILD_DIR%\Stationery.wxs
 %HEAT% dir %SRC_DIR%\FlashImages           %HEAT_OPTIONS% -cg Cg.FlashImages           -dr D.FlashImages           -out %WIX_BUILD_DIR%\FlashImages.wxs
-%HEAT% dir %SRC_DIR%\WizardPatches         %HEAT_OPTIONS% -cg Cg.WizardPatchData       -dr D.WizardPatchData       -out %WIX_BUILD_DIR%\WizardPatchData.wxs
 %HEAT% dir %SRC_DIR%\Examples              %HEAT_OPTIONS% -cg Cg.Examples              -dr D.Examples              -out %WIX_BUILD_DIR%\Examples.wxs
 %HEAT% dir %SRC_DIR%\UpdateSite            %HEAT_OPTIONS% -cg Cg.UpdateSite            -dr MANUFACTURER_FOLDER     -out %WIX_BUILD_DIR%\UpdateSite.wxs
 
 %CANDLE% %CANDLE_OPTIONS% -dProductVersion=%VERSIONn% -dWxWidgetsVer=%WXWIDGETS_VERSION% -dSrcDir="%SRC_DIR%" -o %WIX_BUILD_DIR%\ *.wxs %WIX_BUILD_DIR%\*.wxs
-%LIGHT% %LIGHT_OPTIONS% %LIGHT_DIRS% -out %MSI_FILE% %WIX_BUILD_DIR%\*.wixobj
+%LIGHT% %LIGHT_OPTIONS% %LIGHT_DIRS% -out %MSI_FILE_UTILITY% %WIX_BUILD_DIR%\*.wixobj
 
 goto finish
 :doPatch
 
-%TORCH% -p -xi USBDM_4_9_0_Win.wixpdb %MSI_FILE%.wixpdb -out %PATCH%\diff.wixmst
+%TORCH% -p -xi USBDM_4_9_0_Win.wixpdb %MSI_FILE_UTILITY%.wixpdb -out %PATCH%\diff.wixmst
 
 %CANDLE% %PATCH%.wxs
 %LIGHT%  %PATCH%.wixobj -out %PATCH%\%PATCH%.wixmsp
 %PYRO%   %PATCH%\%PATCH%.wixmsp -out %PATCH%\%PATCH%.msp -t USBDMPatch %PATCH%\diff.wixmst
 
 :finish
-rem pause
+pause
