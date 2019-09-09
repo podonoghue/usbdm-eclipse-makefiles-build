377bc47dcdc974c51a7af88928825c64c4728e08
Chibueze Cyril Akaluka <akalukacyril@gmail.com>
2019-09-09 16:50:23 +0300

    Added ConsoleProgressDialogue class
    The originally available progress dialogue implementation could only work for a GUI implementation.
    
    As the ability to run apps from the command line is desired, a progress dialogue implementation that outputs to stdout is required.
    
    - Added ConsoleProgressDialogue class implementation
    - Added a progress dialogue factory create signature for a ConsoleProgressDialogue

--- Shared_V4/src/ProgressDialogue.cpp
+++ Shared_V4/src/ProgressDialogue.cpp
@@ -66,6 +66,123 @@ public:
    }
 };
 
+class ConsoleProgressDialogue : public ProgressDialogue {
+private:
+   uint32_t          maximum;
+   uint32_t          cumulative;
+   int               currentPercent;
+   int               previousPercent;
+   std::string       updateMessage;
+   bool              verbose;
+   bool              outputEnabled;
+
+   void calculatePercent(int value) {
+      cumulative = value;
+      double percentage = (100.0*value)/maximum;
+      currentPercent = (int)round(percentage);
+      if (currentPercent >= 100) {
+         currentPercent = 99;
+      }
+   }
+
+   bool update(std::string message, bool newUpdateEntry){
+	   if(newUpdateEntry){
+		   if(previousPercent != -1 && !updateMessage.empty()){
+			   fprintf(stdout, "%s complete\n", updateMessage.c_str()); //last update is completed
+		   }
+	       previousPercent = -1;
+	       updateMessage = message;
+	       if(!updateMessage.empty()){
+	    	   fprintf(stdout, "%s\n", updateMessage.c_str());
+	       }
+	   }
+	   else{
+		   if(!message.empty()){
+			   updateMessage = message;
+		   }
+
+		   if(previousPercent != currentPercent && !updateMessage.empty()){
+			   fprintf(stdout, "%d%%: %s\n", currentPercent, updateMessage.c_str());
+		   }
+	   }
+	   previousPercent = currentPercent;
+	   return true;
+   }
+
+public:
+   ConsoleProgressDialogue(uint32_t maximum, bool verbose, bool enableOutput):
+	   maximum(maximum), cumulative(0),
+	   currentPercent(0), previousPercent(-1),
+	   verbose(verbose), outputEnabled(enableOutput)
+    {
+//      LOGGING;
+    }
+   ~ConsoleProgressDialogue() {}
+
+   bool pulse(std::string message=""){
+	   if(outputEnabled && !message.empty()){
+		   fprintf(stdout, "%s\n", message.c_str());
+	   }
+	   return true;
+   }
+
+   bool update(int amount, std::string message=""){
+	   if(!outputEnabled){
+	   	  return true;
+	   }
+	   if(!verbose){
+	      if(!message.empty()){ // print any non-empty message and return
+	         fprintf(stdout, "%s\n", message.c_str());
+	      }
+	      return true;
+	   }
+	   calculatePercent(amount);
+	   return update(message, true);
+   }
+
+   bool incrementalUpdate(int relativeAmount, std::string message=""){
+	   if(!verbose || !outputEnabled){
+		   return true;
+	   }
+	   calculatePercent(cumulative + relativeAmount);
+	   return update(message, false);
+   }
+
+   bool percentageUpdate(int percent, std::string message=""){
+	   if(!outputEnabled){
+		   return true;
+	   }
+	   if(!verbose){
+		   if(percent == 0 && !message.empty()){
+			   fprintf(stdout, "%s\n", message.c_str());
+		   }
+		   return true;
+	   }
+	   this->currentPercent = percent;
+	   updateMessage = message.empty() ? updateMessage : message;
+	   if(previousPercent != currentPercent){
+		  fprintf(stdout, "%d%%: %s\n", currentPercent, updateMessage.c_str());
+	   }
+	   previousPercent = currentPercent;
+	   return true;
+   }
+
+   void setRange(int maximum){this->maximum = maximum;}
+
+   void close(){
+	   if(!verbose || !outputEnabled){
+		   return;
+	   }
+	   if(!updateMessage.empty()){
+		   fprintf(stdout, "%s complete\n", updateMessage.c_str());
+	   }
+   }
+};
+
 ProgressDialoguePtr ProgressDialogueFactory::create(std::string title, uint32_t maximum, void *owner) {
    return ProgressDialoguePtr(new ProgressCallbackImp(title, maximum, (wxWindow*)owner));
+}
+
+ProgressDialoguePtr ProgressDialogueFactory::create(uint32_t maximum, bool verbose, bool enableOutput){
+	return ProgressDialoguePtr(new ConsoleProgressDialogue(maximum, verbose, enableOutput));
 }
