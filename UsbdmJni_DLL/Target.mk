podonoghue/usbdm-eclipse-makefiles-build

commit add56e0fbb472f47832d64a285b391085c82086d

V4.12.1.270 - Updated to using MinGW-64 for all windows buld and libraries

UsbdmJni_DLL/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -22,7 +29,15 @@ SOURCEDIRS := src
 CC = $(GPP)
 
 # Extra Compiler flags
-CFLAGS +=
+CFLAGS += -fno-exceptions
+
+LDFLAGS += $(LFLAGS)
+LDFLAGS += -Wl,--kill-at -shared 
+ifeq ($(UNAME_S),Windows)
+LDFLAGS += 
+else	
+LDFLAGS += -Wl,-soname,$(basename $(notdir $@))
+endif
 
 # Extra C Definitions
 DEFS += $(CDEFS)  # From command line
@@ -30,17 +45,17 @@ DEFS +=
 
 # Look for include files in each of the modules
 INCS := $(patsubst %,-I%,$(SOURCEDIRS))
-INCS += 
+INCS +=  $(JAVA_INC) 
 
 # Look for include files in each of the modules (don't include JAVA dirs)
-WINDRES_INCS := $(patsubst %,-I%,$(SOURCEDIRS))
+#WINDRES_INCS := $(patsubst %,-I%,$(SOURCEDIRS))
 
 # Extra Library dirs
 LIBDIRS += 
 
 # Extra libraries
 LIBS += $(USBDM_LIBS) 
-LIBS += 
+LIBS +=
 
 # Each module will add to this
 SRC :=
@@ -77,11 +92,11 @@ endif
 
 $(BUILDDIR)/%.o : %.c
 	@echo -- Building $@ from $<
-	$(CC) $(CFLAGS) $(DEFS) $(INCS) $(JAVA_INC) -MD -c $< -o $@
+	$(CC) $(CFLAGS) $(DEFS) $(INCS) -MD -c $< -o $@
 	
 $(BUILDDIR)/%.o : %.cpp
 	@echo -- Building $@ from $<
-	$(CC) $(CFLAGS) $(DEFS) $(INCS) $(JAVA_INC) -MD -c $< -o $@
+	$(CC) $(CFLAGS) $(DEFS) $(INCS) -MD -c $< -o $@
 	
 # How to link an EXE
 #==============================================
@@ -104,7 +119,7 @@ $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
 	@echo -- Linking Target $@
 ifeq ($(UNAME_S),Windows)
-	$(CC) -Wl,--kill-at -shared $(STATIC_GCC_OPTION) -o $@  $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) 
+	$(CC) -o $@  $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) 
 else	
 	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) 
 endif
@@ -126,18 +141,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+   
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -149,9 +153,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

BdmInterface_DLL/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -37,11 +44,11 @@ LIBDIRS +=
 
 # Extra libraries
 ifneq ($(UNAME_S),Windows)
-LIBS += -ldl
 LIBS += -lm
 endif
 LIBS += $(USBDM_SYSTEM_LIBS)
 LIBS += $(USBDM_LIBS)
+LIBS += $(USBDM_DYNAMIC_LIBS)
 
 # Extra libraries for EXE only
 EXELIBS += 
@@ -126,18 +133,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -149,9 +145,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

CreateCTestImage/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -26,7 +33,7 @@ CFLAGS +=
 
 # Extra C Definitions
 DEFS += $(CDEFS)  # From command line
-DEFS +=
+DEFS += 
 
 # Look for include files in each of the modules
 INCS := $(patsubst %,-I%,$(SOURCEDIRS))
@@ -99,8 +106,8 @@ $(TARGET_BINDIR)/$(TARGET_EXE): $(BUILDDIR)/$(TARGET_EXE)
 $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
 	@echo -- Linking Target $@
-	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) 
-
+	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS)
+	
 # How to copy LIBRARY to target directory
 #==============================================
 $(TARGET_LIBDIR)/$(TARGET_DLL): $(BUILDDIR)/$(TARGET_DLL)
@@ -118,18 +125,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -141,9 +137,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

CreateFlashTestImage/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -26,7 +33,7 @@ CFLAGS +=
 
 # Extra C Definitions
 DEFS += $(CDEFS)  # From command line
-DEFS +=
+DEFS += 
 
 # Look for include files in each of the modules
 INCS := $(patsubst %,-I%,$(SOURCEDIRS))
@@ -99,8 +106,8 @@ $(TARGET_BINDIR)/$(TARGET_EXE): $(BUILDDIR)/$(TARGET_EXE)
 $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
 	@echo -- Linking Target $@
-	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) 
-
+	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS)
+	
 # How to copy LIBRARY to target directory
 #==============================================
 $(TARGET_LIBDIR)/$(TARGET_DLL): $(BUILDDIR)/$(TARGET_DLL)
@@ -118,18 +125,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -141,9 +137,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

DeviceDatabase_DLL/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -37,15 +44,14 @@ LIBDIRS += $(XERCES_LIBDIRS)
 
 # Extra libraries
 ifneq ($(UNAME_S),Windows)
-LIBS += -ldl
 LIBS += -lm
 endif
-
+LIBS += $(USBDM_DYNAMIC_LIBS)
+LIBS += $(USBDM_SYSTEM_LIBS)
 LIBS += $(XERCES_LIBS)
 
-LIBS += $(USBDM_SYSTEM_LIBS)
-
 # Extra libraries for EXE only
+EXELIBS += 
 
 # Each module will add to this
 SRC :=
@@ -93,7 +99,7 @@ $(BUILDDIR)/%.o : %.cpp
 $(BUILDDIR)/$(TARGET_EXE): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
 	@echo -- Linking Target $@
-	$(CC) -o $@  $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) ${EXELIBS}
+	$(CC) -o $@ $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) $(EXELIBS)
 
 # How to copy EXE to target directory
 #==============================================
@@ -108,8 +114,8 @@ $(TARGET_BINDIR)/$(TARGET_EXE): $(BUILDDIR)/$(TARGET_EXE)
 $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
 	@echo -- Linking Target $@
-	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) ${EXELIBS}
-
+	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) # ${EXELIBS}
+	
 # How to copy LIBRARY to target directory
 #==============================================
 $(TARGET_LIBDIR)/$(TARGET_DLL): $(BUILDDIR)/$(TARGET_DLL)
@@ -127,18 +133,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -150,9 +145,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

FirmwareChanger/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -27,6 +34,7 @@ CFLAGS +=
 # Extra C Definitions
 DEFS += $(CDEFS)  # From command line
 DEFS += $(WXWIDGETS_DEFS)
+DEFS += 
 
 # Look for include files in each of the modules
 INCS := $(patsubst %,-I%,$(SOURCEDIRS))
@@ -36,9 +44,9 @@ INCS += $(WXWIDGETS_INC)
 LIBDIRS += $(WXWIDGETS_LIBDIRS)
 
 # Extra libraries
-LIBS += $(USBDM_LIBS) 
 LIBS += $(WXWIDGETS_LIBS)
 LIBS += $(USBDM_SYSTEM_LIBS)
+LIBS += $(USBDM_LIBS) 
 LIBS += $(USBDM_DYNAMIC_LIBS)
 
 # Each module will add to this
@@ -86,7 +94,7 @@ $(BUILDDIR)/%.o : %.cpp
 #==============================================
 $(BUILDDIR)/$(TARGET_EXE): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
-	@echo -- Linking Executable $@
+	@echo -- Linking Target $@
 	$(CC) -o $@ $(GUI_OPTS) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) $(EXELIBS)
 
 # How to copy EXE to target directory
@@ -101,9 +109,9 @@ $(TARGET_BINDIR)/$(TARGET_EXE): $(BUILDDIR)/$(TARGET_EXE)
 #==============================================
 $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
-	@echo -- Linking Library $@
+	@echo -- Linking Target $@
 	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS)
-
+	
 # How to copy LIBRARY to target directory
 #==============================================
 $(TARGET_LIBDIR)/$(TARGET_DLL): $(BUILDDIR)/$(TARGET_DLL)
@@ -121,18 +129,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -144,9 +141,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

FlashImage_DLL/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -37,11 +44,11 @@ LIBDIRS +=
 
 # Extra libraries
 ifneq ($(UNAME_S),Windows)
-LIBS += -ldl
 LIBS += -lm
 endif
-LIBS += $(USBDM_LIBS) 
+LIBS += $(USBDM_DYNAMIC_LIBS)
 LIBS += $(USBDM_SYSTEM_LIBS)
+LIBS += $(USBDM_LIBS) 
 
 # Extra libraries for EXE only
 EXELIBS += 
@@ -108,7 +115,7 @@ $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
 	@echo -- Linking Target $@
 	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) ${EXELIBS}
-
+	
 # How to copy LIBRARY to target directory
 #==============================================
 $(TARGET_LIBDIR)/$(TARGET_DLL): $(BUILDDIR)/$(TARGET_DLL)
@@ -124,19 +131,9 @@ endif
 # Create required directories for targets
 #==============================================
 $(BUILDDIR) :
-	@echo -- Making BUILDDIR directory $(BUILDDIR)
+	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making TARGET_LIBDIR directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making TARGET_BINDIR directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -150,9 +147,9 @@ $(BUILDDIR)/$(TARGET_EXE) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

GDI_DLL/Target.mk
@@ -3,17 +3,24 @@
 #CDEFS     = -DLOG
 #MODULE    = module
 #TARGET    = BUILDDIR
-
-# Makefiles in subdirs used to collect targets (default 'module.mk')
-MODULE ?= module
-
-# Main target name (default same as build directory)
-TARGET ?= $(BUILDDIR)
-
-TARGET_DLL=$(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
-TARGET_EXE=$(TARGET)$(EXE_SUFFIX)
+#LFLAGS    = 
 
 include ../Common.mk
+
+# Makefiles in subdirs used to collect targets (default 'module.mk')
+ifeq ($(origin MODULE), undefined)
+	MODULE := module
+endif
+
+# Main target name (default same as build directory)
+ifeq ($(origin TARGET), undefined)
+	TARGET := $(BUILDDIR)$(VSUFFIX)
+endif
+
+TARGET_DLL = $(LIB_PREFIX)$(TARGET)$(LIB_SUFFIX)
+TARGET_EXE = $(TARGET)$(EXE_SUFFIX)
+
+override BUILDDIR:=$(BUILDDIR)$(BUILDDIR_SUFFIX)
 
 VPATH      := src $(BUILDDIR) 
 SOURCEDIRS := src
@@ -26,29 +33,21 @@ CFLAGS +=
 
 # Extra C Definitions
 DEFS += $(CDEFS)  # From command line
-DEFS += $(XERCES_DEFS)
+#DEFS += $(XERCES_DEFS)
 
 # Look for include files in each of the modules
 INCS := $(patsubst %,-I%,$(SOURCEDIRS))
-#INCS += $(XERCES_INC)
 
 # Extra Library dirs
-LIBDIRS += $(XERCES_LIBDIRS)
+#LIBDIRS += $(XERCES_LIBDIRS)
 
 # Extra libraries
-ifneq ($(UNAME_S),Windows)
-LIBS += -ldl
-LIBS += -lm
-endif
-#LIBS += $(USBDM_LIBS) 
-LIBS += $(USBDM_DEVICE_LIBS)
+LIBS += $(USBDM_DYNAMIC_LIBS)
 LIBS += $(USBDM_SYSTEM_LIBS)
-#LIBS += $(USBDM_WX_LIBS)
-#LIBS += $(USBDM_PROGRAMMERS_LIBS)
-#LIBS += $(FLASHIMAGE_LIBS)
+LIBS += $(USBDM_DEVICE_LIBS)
 
 # Extra libraries for EXE only
-EXELIBS += $(USBDM_DYNAMIC_LIBS)
+EXELIBS += 
 
 # Each module will add to this
 SRC :=
@@ -95,7 +94,7 @@ $(BUILDDIR)/%.o : %.cpp
 #==============================================
 $(BUILDDIR)/$(TARGET_EXE): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
-	@echo -- Linking Executable $@
+	@echo -- Linking Target $@
 	$(CC) -o $@ $(GUI_OPTS) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS) $(EXELIBS)
 
 # How to copy EXE to target directory
@@ -110,9 +109,9 @@ $(TARGET_BINDIR)/$(TARGET_EXE): $(BUILDDIR)/$(TARGET_EXE)
 #==============================================
 $(BUILDDIR)/$(TARGET_DLL): $(OBJ) $(RESOURCE_OBJ)
 	@echo --
-	@echo -- Linking Library $@
+	@echo -- Linking Target $@
 	$(CC) -shared -o $@ -Wl,-soname,$(basename $(notdir $@)) $(LDFLAGS) $(OBJ) $(RESOURCE_OBJ) $(LIBDIRS) $(LIBS)
-
+	
 # How to copy LIBRARY to target directory
 #==============================================
 $(TARGET_LIBDIR)/$(TARGET_DLL): $(BUILDDIR)/$(TARGET_DLL)
@@ -130,18 +129,7 @@ endif
 $(BUILDDIR) :
 	@echo -- Making directory $(BUILDDIR)
 	-$(MKDIR) $(BUILDDIR)
-    
-ifneq ($(TARGET_LIBDIR),$(TARGET_BINDIR))
-$(TARGET_LIBDIR) :
-	@echo -- Making directory $(TARGET_LIBDIR)
-	-$(MKDIR) $(TARGET_LIBDIR)
-    
-endif
-
-$(TARGET_BINDIR) :
-	@echo -- Making directory $(TARGET_BINDIR)
-	-$(MKDIR) $(TARGET_BINDIR)
-    
+       
 $(TARGET_LIBDIR)/$(TARGET_DLL): | $(TARGET_LIBDIR)
 
 $(TARGET_BINDIR)/$(TARGET_EXE): | $(TARGET_BINDIR)
@@ -153,9 +141,9 @@ $(BUILDDIR)/$(TARGET_DLL) $(OBJ) $(RESOURCE_OBJ): | $(BUILDDIR)
 clean:
 	-$(RMDIR) $(BUILDDIR)
 
-dll: $(TARGET_LIBDIR)/$(TARGET_DLL)
+dll: commonFiles $(TARGET_LIBDIR)/$(TARGET_DLL)
 
-exe: $(TARGET_BINDIR)/$(TARGET_EXE)
+exe: commonFiles $(TARGET_BINDIR)/$(TARGET_EXE)
    
 .PHONY: clean dll exe
 

commit 02bfcc9efbc83e5f4605810e833913d58bb002f3

Only pass --kill-at linker flag when on Windows
commit add56e0fbb47 ("V4.12.1.270 - Updated to using MinGW-64 for all
windows buld and libraries") added this linker flag unconditionally,
even though it's only valid on Windows.

Fixes: add56e0fbb47 ("V4.12.1.270 - Updated to using MinGW-64 for all windows buld and libraries")

UsbdmJni_DLL/Target.mk
@@ -32,9 +32,9 @@ CC = $(GPP)
 CFLAGS += -fno-exceptions
 
 LDFLAGS += $(LFLAGS)
-LDFLAGS += -Wl,--kill-at -shared 
+LDFLAGS += -shared
 ifeq ($(UNAME_S),Windows)
-LDFLAGS += 
+LDFLAGS += -Wl,--kill-at
 else	
 LDFLAGS += -Wl,-soname,$(basename $(notdir $@))
 endif